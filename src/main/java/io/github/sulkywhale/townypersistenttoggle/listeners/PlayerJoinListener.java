5b9c5b9eb1058e1fd141f7c099c3d84d6ee74e24
Sulky_Whale <171066059+SulkyWhale@users.noreply.github.com>
2025-06-23 08:00:07 +0200

    Fix resident modes throwing permission error

--- src/main/java/io/github/sulkywhale/townypersistenttoggle/listeners/PlayerJoinListener.java
+++ src/main/java/io/github/sulkywhale/townypersistenttoggle/listeners/PlayerJoinListener.java
@@ -1,5 +1,6 @@
 package io.github.sulkywhale.townypersistenttoggle.listeners;
 
+import com.palmergames.bukkit.towny.Towny;
 import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.exceptions.TownyException;
 import com.palmergames.bukkit.towny.huds.HUDManager;
@@ -8,6 +9,7 @@ import com.palmergames.bukkit.towny.object.resident.mode.ResidentModeHandler;
 import io.github.sulkywhale.townypersistenttoggle.metadata.MetadataController;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -15,19 +17,19 @@ import java.util.List;
 
 public class PlayerJoinListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         Resident resident = TownyAPI.getInstance().getResident(player);
         List<String> modes = MetadataController.getModesData(resident);
         String hud = MetadataController.getHUDsData(resident);
-        for (String mode : modes) {
-            try {
-                ResidentModeHandler.toggleMode(resident, mode, false);
-            } catch (TownyException e) {
-                event.getPlayer().sendMessage(e.getMessage());
+        Towny.getPlugin().getScheduler().runLater(() -> {
+            for (String mode : modes) {
+                try {
+                    ResidentModeHandler.toggleMode(resident, mode, false);
+                } catch (TownyException ignored) {}
             }
-        }
+        }, 2L);
         if (hud.equalsIgnoreCase("maphud")) {
             HUDManager.toggleMapHud(player);
         } else if (hud.equalsIgnoreCase("permhud")) {
